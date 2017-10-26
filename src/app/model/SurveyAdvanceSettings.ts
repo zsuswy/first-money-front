9e117be02eaa32253e10f64b957e12312fa6d22f
Jeffrey Sun <zsuswy@gmail.com>
2017-10-26 23:33:19 +0800

    推进结果显示页的进度

---
+++ src/app/model/SurveyAdvanceSettings.ts
@@ -0,0 +1,6 @@
+export class SurveyAdvanceSettings {
+    constructor(public description?: string,            // 测评描述
+                public templateType?: number            // 模版类型
+    ) {
+    }
+}
